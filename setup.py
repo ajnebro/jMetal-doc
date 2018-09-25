b5ba2a6463e53a2a846709beae4e0d35bedf6aa7
benhid <makishinake@hotmail.com>
2018-09-25 10:41:34 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='jmetal',
+    version='0.5.1',
+    description='JMetal Docs',
+    author='Antonio J. Nebro',
+    author_email='antonio@lcc.uma.es',
+    maintainer='Antonio J. Nebro, Antonio Benítez-Hidalgo',
+    maintainer_email='antonio@lcc.uma.es, antonio.b@uma.es',
+    license='MIT',
+    url='https://github.com/benhid/jMetal-doc'
+)
